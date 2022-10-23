2b4d22b08df3adc0d9198823c5d0fffd8c2a6327
Marcos <mlopez@cifpfbmoll.eu>
2022-10-23 05:30:00 +0200

    Aplicación acabada, falta ponerle un icono y el README

---
+++ .idea/sonarlint/issuestore/index.pb
@@ -0,0 +1,9 @@
+
+9
+	README.md,8\e\8ec9a00bfd09b3190ac6b22251dbb1aa95a0579d
+g
+7app/src/main/java/com/example/task12b/MainActivity.java,1\1\11f721f9e439091df5630f513127ee89da10682e
+q
+Aapp/src/main/java/com/example/task12b/RelativeLayoutActivity.java,2\5\25022c2a7f233b7c5cecb3d2c534f72aae37966b
+s
+Capp/src/main/java/com/example/task12b/ConstraintLayoutActivity.java,3\1\31fcb275c6845fb2d12b7c2058594960897e8ed0
---
+++ app/src/main/java/com/example/task12b/RelativeLayoutActivity.java
@@ -0,0 +1,85 @@
+package com.example.task12b;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class RelativeLayoutActivity extends AppCompatActivity {
+
+    private Button buttonToast;
+    private Button buttonCount;
+    private TextView showCount;
+    private Button goToLinearLayout;
+    private Button goToConstraintLayout;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_relative_layout);
+
+
+        this.buttonToast = findViewById(R.id.button_toast);
+        this.buttonCount = findViewById(R.id.button_count);
+        this.showCount = findViewById(R.id.show_count);
+        this.goToLinearLayout = findViewById(R.id.goToLinearLayout);
+        this.goToConstraintLayout = findViewById(R.id.goToConstraintLayout);
+
+
+
+        Bundle extras = getIntent().getExtras();
+        if (extras != null) {
+            this.showCount.setText(extras.getString("count"));
+        }
+
+        if (savedInstanceState != null) {
+            this.showCount.setText(savedInstanceState.getString("numberCount"));
+        }
+
+
+
+        this.buttonCount.setOnClickListener(view ->
+                this.sumCount()
+        );
+
+        this.buttonToast.setOnClickListener(view ->
+                this.showToastCount()
+        );
+
+        this.goToLinearLayout.setOnClickListener(view -> {
+            Intent intent = new Intent(this, MainActivity.class);
+            intent.putExtra("count", this.showCount.getText());
+            this.startActivity(intent);
+        });
+
+        this.goToConstraintLayout.setOnClickListener(view -> {
+            Intent intent = new Intent(this, ConstraintLayoutActivity.class);
+            intent.putExtra("count", this.showCount.getText());
+            this.startActivity(intent);
+        });
+
+    }
+
+    public void sumCount() {
+        int sum = Integer.parseInt((String) this.showCount.getText()) + 1;
+        this.showCount.setText(String.valueOf(sum));
+
+    }
+
+
+    public void showToastCount() {
+        Toast.makeText(this, showCount.getText(), Toast.LENGTH_SHORT).show();
+    }
+
+
+    @Override
+    protected void onSaveInstanceState(Bundle savedInstanceState) {
+        super.onSaveInstanceState(savedInstanceState);
+        savedInstanceState.putString("numberCount", (String) this.showCount.getText());
+
+    }
+}
